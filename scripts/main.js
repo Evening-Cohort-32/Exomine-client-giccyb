45c38fd86b32840d745ad47a68739a0a4f54ef53
Jason LeGlue <jasonleglue@gmail.com>
2026-05-16 15:51:27 -0500

    Created governorList() in governors.js, and created render() in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
 //Invoke mineralShop function and display it's html in <main id="content">
+
+import { mineralShopHTML } from "./mineralShop.js";
+
+const content = document.getElementById("content");
+
+const render = async () => {
+  content.innerHTML = await mineralShopHTML();
+};
+
+render();
