a97cdf432fc5733d9986fbbffecd4d979ed76690
Jason LeGlue <jasonleglue@gmail.com>
2026-05-16 15:58:07 -0500

    Connected mineralShop.js with main.js and governors.js

--- scripts/governors.js
+++ scripts/governors.js
@@ -1,16 +1,10 @@
 //Dropdown menu for Governor choices
 
 export const governorList = async () => {
-  // fetch gov list
-  //make it readable/ not json
-  // map it onto new array
-  // join .map array
-  // add it to html to be returned
-
   const response = await fetch("http://localhost:8088/governors");
   const governors = await response.json();
 
-  let html = `<label for="governors">Choose a governor</label><select name="governors" id="governors">`;
+  let html = `<label for="governors">Choose a governor </label><select name="governors" id="governors">`;
 
   const governorArray = await governors.map((gov) => {
     return `<option value="${gov.id}">${gov.name}</option>`;
--- scripts/mineralShop.js
+++ scripts/mineralShop.js
@@ -2,31 +2,31 @@
 //Creates HTML for the entire mineral shop webpage
 
 import { governorList } from "./governors.js";
-import { coloniesList } from "./colonies.js";
-import { inventoryMinerals } from "./inventoryMinerals.js";
-import { availableMinerals } from "./availableMinerals.js";
-import { submitButton } from "./submitButton.js";
+// import { coloniesList } from "./colonies.js";
+// import { inventoryMinerals } from "./inventoryMinerals.js";
+// import { availableMinerals } from "./availableMinerals.js";
+// import { submitButton } from "./submitButton.js";
 
-export const MineralShop = async () => {
+export const mineralShop = async () => {
   return `
     <h1>Solar System Mining Market</h1>
 
     <section class="top-row">
       <div class="selectors">
         ${await governorList()}
-        ${await inventoryMinerals()}
+        
       </div>
       <div class="colony-display">
-        ${await coloniesList()}
+        
       </div>
     </section>
 
 <section class="bottom-row">
       <div class="minerals-display">
-        ${await availableMinerals()}
+        
       </div>
       <div class="cart-display">
-        ${submitButton()}
+        
       </div>
     </section>
 
