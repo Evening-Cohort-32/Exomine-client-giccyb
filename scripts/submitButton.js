e48e0fa293ada731ebbf92ee24530aece92680d0
mramey-dev <rameykenya93@gmail.com>
2026-05-18 19:52:44 -0500

    WIP: update to database.js and submitButton.js started

--- scripts/submitButton.js
+++ scripts/submitButton.js
@@ -1,1 +1,13 @@
 //HTML for generating "Purchase Mineral" button
+
+const handleOrderSubmission = (clickEvent) => {
+  if (clickEvent.target.id === "submit-button") {
+    console.log("Button clicked!");
+  }
+};
+
+export const submitButton = () => {
+  document.addEventListener("click", handleOrderSubmission);
+
+  return `<button id="submit-button">Purchase Mineral</button>`;
+};
