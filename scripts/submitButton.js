b5a17d393fa308c5581c3dc2d463702bf21c39cd
mramey-dev <rameykenya93@gmail.com>
2026-05-19 21:15:43 -0500

    added submitButton.js

--- scripts/submitButton.js
+++ scripts/submitButton.js
@@ -1,13 +1,70 @@
+//This module handles the "Purchase" click
 //HTML for generating "Purchase Mineral" button
+import { getState } from "./TransientState.js";
 
-const handleOrderSubmission = (clickEvent) => {
+export const handleOrderSubmission = async (clickEvent) => {
   if (clickEvent.target.id === "submit-button") {
-    console.log("Button clicked!");
+    const state = getState();
+
+    const governorResp = await fetch(
+      `http://localhost:8088/governors/${state.selectedGovernor}`,
+    );
+    const governor = await governorResp.json();
+
+    const facilityMineralResp = await fetch(
+      `http://localhost:8088/facilityMinerals?facilityId=${state.selectedFacility}&mineralId=${state.selectedMineral}`,
+    );
+    const facilityMinerals = await facilityMineralResp.json();
+    const facilityMineral = facilityMinerals[0];
+
+    if (!facilityMineral || facilityMineral.quantity <= 0) {
+      return;
+    }
+
+    const colonyMineralResp = await fetch(
+      `http://localhost:8088/colonyMinerals?colonyId=${governor.colonyId}&mineralId=${state.selectedMineral}`,
+    );
+    const colonyMinerals = await colonyMineralResp.json();
+    const colonyMineral = colonyMinerals[0];
+
+    await fetch(
+      `http://localhost:8088/facilityMinerals/${facilityMineral.id}`,
+      {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          quantity: facilityMineral.quantity - 1,
+        }),
+      },
+    );
+
+    if (colonyMineral) {
+      await fetch(`http://localhost:8088/colonyMinerals/${colonyMineral.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          quantity: colonyMineral.quantity + 1,
+        }),
+      });
+    } else {
+      await fetch(`http://localhost:8088/colonyMinerals`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          colonyId: governor.colonyId,
+          mineralId: state.selectedMineral,
+          quantity: 1,
+        }),
+      });
+    }
+    document.dispatchEvent(new CustomEvent("stateChanged"));
   }
 };
 
 export const submitButton = () => {
-  document.addEventListener("click", handleOrderSubmission);
-
   return `<button id="submit-button">Purchase Mineral</button>`;
 };
