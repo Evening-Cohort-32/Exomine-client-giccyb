9ef0ca3991ddc626ec9e9febca4d9b1f481cb5b9
Jason LeGlue <jasonleglue@gmail.com>
2026-05-19 19:51:35 -0500

    Fixed dropdown bug

--- scripts/governors.js
+++ scripts/governors.js
@@ -1,13 +1,20 @@
 //Dropdown menu for Governor choices
+import { getState } from "./TransientState.js";
 
 export const governorList = async () => {
   const response = await fetch("http://localhost:8088/governors");
   const governors = await response.json();
 
+  const state = getState();
+
   let html = `<label for="governors">Choose a governor </label><select name="governors" id="governors"><option value="0">Choose a governor...</option>`;
 
   const governorArray = governors.map((gov) => {
-    return `<option value="${gov.id}" data-colony="${gov.colonyId}">${gov.name}</option>`;
+    if (parseInt(state.selectedGovernor) === parseInt(gov.id)) {
+      return `<option value="${gov.id}" data-colony="${gov.colonyId}" selected>${gov.name}</option>`;
+    } else {
+      return `<option value="${gov.id}" data-colony="${gov.colonyId}">${gov.name}</option>`;
+    }
   });
 
   html += `${governorArray.join("")}</select>`;
